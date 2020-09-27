95f66c8a37f5acb1c0a66a31a487263573dd84d1
Tomasz Szczodrowski <bdn583@gmail.com>
2020-09-27 11:13:36 +0200

    Add FailTest

---
+++ src/test/java/sda/FailTest.java
@@ -0,0 +1,14 @@
+package sda;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class FailTest {
+
+    @Test
+    void failedTest() {
+        // something something
+        Assertions.fail("failed manually");
+    }
+
+}
